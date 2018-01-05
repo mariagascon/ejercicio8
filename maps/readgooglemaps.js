bf8af548ed57a5ddbc6cec3c79153a45fbcf138a
Your Name <you@example.com>
2018-01-05 09:02:37 +0100

    *COMMIT FINAL (05/01/2018)

--- maps/readgooglemaps.js
+++ maps/readgooglemaps.js
@@ -18,16 +18,23 @@ function viewJSON() {
         /* Aqui l'objecte response representa l'objecte JSON que ens 
            ha retornat el servidor */
         
-        document.getElementById("tabla").innerHTML = "<tr><th>Nombre</th><th>Dirección</th><th>Latitud</th><th>Longitud</th><th>Tipo de establecimiento</th><th>Icono</th></tr>";
+        document.getElementById("tabla").innerHTML = "<tr class='encabezado'><th class='name'>Nombre</th><th class='vericity'>Dirección</th><th class='lat'>Latitud</th><th class='long'>Longitud</th><th class='tip'>Tipo de establecimiento</th><th class='icon'>Icono</th></tr>";
+        /* Introduce dentro del elemento "tabla" la información que se encuentra
+        tras el = */
 
         for (i=0; i<response.results.length; i++) {
-            document.getElementById("tabla").innerHTML += 
-            "<tr><td>"+response.results[i].name+"</td>"+
-            "<td>"+response.results[i].vicinity+"</td>"+
-            "<td>"+response.results[i].geometry.location.lat+"</td>"+
-            "<td>"+response.results[i].geometry.location.lng+"</td>"+
-            "<td>"+response.results[i].types+"</td>"+
-            "<td><img src=' "+response.results[i].icon+" '></td></tr>";
+        /* Este 'for' recorre todos los resultados con el objetivo de imprimir
+        aquellos que se encuentren entre la posición 0 y la última posición */
+            document.getElementById("tabla").innerHTML +=
+            /* Introduce dentro del elemento "tabla" la información que se encuentra
+            tras el =; en este caso todos los resultados correspondientes a nombre, dirección, 
+            longitud y latitud, tipología e iconos los introduce a través de filas y celdas*/ 
+            "<tr><td class='nombre'>"+response.results[i].name+"</td>"+
+            "<td class='direccion'>"+response.results[i].vicinity+"</td>"+
+            "<td class='latitud'>"+response.results[i].geometry.location.lat+"</td>"+
+            "<td class='longitud'>"+response.results[i].geometry.location.lng+"</td>"+
+            "<td class='tipologia'>"+response.results[i].types+"</td>"+
+            "<td class='icono'><img src=' "+response.results[i].icon+" '></td></tr>";
         }   
     });
 }
